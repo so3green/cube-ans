0d1a2363e0892b00cd0dc1ae69c487323a4dd757
so3green <so3smile@yahoo.co.jp>
2024-09-28 02:00:15 +0900

    add

--- cube-2.c
+++ cube-2.c
@@ -23,18 +23,18 @@ int turn_Tx_Plus(){
             cube_save[i][j] = cube[i][j];
         }
     }
-    cube[0][2]=cube_save[3][0];
-    cube[0][3]=cube_save[3][2];
-    cube[1][3]=cube_save[0][2];
-    cube[1][1]=cube_save[0][3];
-    cube[5][1]=cube_save[1][3];
-    cube[5][0]=cube_save[1][1];
-    cube[3][0]=cube_save[5][1];
-    cube[3][2]=cube_save[5][0];
-    cube[2][0]=cube_save[2][1];
-    cube[2][1]=cube_save[2][3];
-    cube[2][2]=cube_save[2][0];
-    cube[2][3]=cube_save[2][2];
+    cube[4][3]=cube_save[3][1];
+    cube[4][2]=cube_save[3][3];
+    cube[1][2]=cube_save[4][3];
+    cube[1][0]=cube_save[4][2];
+    cube[5][0]=cube_save[1][2];
+    cube[5][1]=cube_save[1][0];
+    cube[3][1]=cube_save[5][0];
+    cube[3][3]=cube_save[5][1];
+    cube[0][1]=cube_save[0][0];
+    cube[0][0]=cube_save[0][2];
+    cube[0][3]=cube_save[0][1];
+    cube[0][2]=cube_save[0][3];
 }
 int turn_Tx_minus(){
     for(int i=0;i<6;i++){
@@ -43,18 +43,18 @@ int turn_Tx_minus(){
             cube_save[i][j] = cube[i][j];
         }
     }
-    cube[0][2]=cube_save[1][3];
-    cube[0][3]=cube_save[1][1];
-    cube[1][3]=cube_save[5][1];
-    cube[1][1]=cube_save[5][0];
-    cube[5][1]=cube_save[3][0];
-    cube[5][0]=cube_save[3][2];
-    cube[3][0]=cube_save[0][2];
-    cube[3][2]=cube_save[0][3];
-    cube[2][0]=cube_save[2][2];
-    cube[2][1]=cube_save[2][0];
-    cube[2][2]=cube_save[2][3];
-    cube[2][3]=cube_save[2][1];
+    cube[4][3]=cube_save[1][2];
+    cube[4][2]=cube_save[1][0];
+    cube[1][2]=cube_save[5][0];
+    cube[1][0]=cube_save[5][1];
+    cube[5][0]=cube_save[3][1];
+    cube[5][1]=cube_save[3][3];
+    cube[3][1]=cube_save[4][3];
+    cube[3][3]=cube_save[4][2];
+    cube[0][1]=cube_save[0][3];
+    cube[0][0]=cube_save[0][1];
+    cube[0][3]=cube_save[0][2];
+    cube[0][2]=cube_save[0][0];
 }
 int turn_Ty_Plus(){
     for(int i=0;i<6;i++){
@@ -63,18 +63,18 @@ int turn_Ty_Plus(){
             cube_save[i][j] = cube[i][j];
         }
     }
-    cube[4][1]=cube_save[2][2];
-    cube[4][3]=cube_save[2][0];
-    cube[0][1]=cube_save[4][1];
-    cube[0][3]=cube_save[4][3];
-    cube[5][1]=cube_save[0][1];
-    cube[5][3]=cube_save[0][3];
-    cube[2][2]=cube_save[5][1];
-    cube[2][0]=cube_save[5][3];
-    cube[3][0]=cube_save[3][1];
-    cube[3][1]=cube_save[3][3];
-    cube[3][2]=cube_save[3][0];
-    cube[3][3]=cube_save[3][2];
+    cube[4][0]=cube_save[2][3];
+    cube[4][2]=cube_save[2][1];
+    cube[0][0]=cube_save[4][0];
+    cube[0][2]=cube_save[4][2];
+    cube[5][0]=cube_save[0][0];
+    cube[5][2]=cube_save[0][2];
+    cube[2][3]=cube_save[5][0];
+    cube[2][1]=cube_save[5][2];
+    cube[3][1]=cube_save[3][0];
+    cube[3][0]=cube_save[3][2];
+    cube[3][3]=cube_save[3][1];
+    cube[3][2]=cube_save[3][3];
 }
 int turn_Ty_minus(){
     for(int i=0;i<6;i++){
@@ -83,18 +83,18 @@ int turn_Ty_minus(){
             cube_save[i][j] = cube[i][j];
         }
     }//0>4,2>0,4>2,
-    cube[4][1]=cube_save[0][1];
-    cube[4][3]=cube_save[0][3];
-    cube[0][1]=cube_save[5][1];
-    cube[0][3]=cube_save[5][3];
-    cube[5][1]=cube_save[2][2];
-    cube[5][3]=cube_save[2][0];
-    cube[2][2]=cube_save[4][1];
-    cube[2][0]=cube_save[4][3];
-    cube[3][0]=cube_save[3][2];
-    cube[3][1]=cube_save[3][0];
-    cube[3][2]=cube_save[3][3];
-    cube[3][3]=cube_save[3][1];
+    cube[4][0]=cube_save[0][0];
+    cube[4][2]=cube_save[0][2];
+    cube[0][0]=cube_save[5][0];
+    cube[0][2]=cube_save[5][2];
+    cube[5][0]=cube_save[2][3];
+    cube[5][2]=cube_save[2][1];
+    cube[2][3]=cube_save[4][0];
+    cube[2][1]=cube_save[4][2];
+    cube[3][1]=cube_save[3][3];
+    cube[3][0]=cube_save[3][0];
+    cube[3][3]=cube_save[3][2];
+    cube[3][2]=cube_save[3][0];
 }
 int turn_Tz_Plus(){
     for(int i=0;i<6;i++){
@@ -103,18 +103,18 @@ int turn_Tz_Plus(){
             cube_save[i][j] = cube[i][j];
         }
     }
-    cube[0][0]=cube_save[0][1];
-    cube[0][1]=cube_save[0][3];
-    cube[0][2]=cube_save[0][0];
-    cube[0][3]=cube_save[0][2];
-    cube[2][0]=cube_save[1][0];
-    cube[2][1]=cube_save[1][1];
-    cube[3][0]=cube_save[2][0];
-    cube[3][1]=cube_save[2][1];
-    cube[4][0]=cube_save[3][0];
-    cube[4][1]=cube_save[3][1];
-    cube[1][0]=cube_save[4][0];
-    cube[1][1]=cube_save[4][1];
+    cube[4][0]=cube_save[4][1];
+    cube[4][1]=cube_save[4][3];
+    cube[4][2]=cube_save[4][0];
+    cube[4][3]=cube_save[4][2];
+    cube[0][0]=cube_save[1][0];
+    cube[0][1]=cube_save[1][1];
+    cube[3][0]=cube_save[0][0];
+    cube[3][1]=cube_save[0][1];
+    cube[2][0]=cube_save[3][0];
+    cube[2][1]=cube_save[3][1];
+    cube[1][0]=cube_save[2][0];
+    cube[1][1]=cube_save[2][1];
 }
 int turn_Tz_minus(){
     for(int i=0;i<6;i++){
@@ -123,21 +123,22 @@ int turn_Tz_minus(){
             cube_save[i][j] = cube[i][j];
         }
     }
-    cube[0][0]=cube_save[0][2];
-    cube[0][1]=cube_save[0][0];
-    cube[0][2]=cube_save[0][3];
-    cube[0][3]=cube_save[0][1];
-    cube[2][0]=cube_save[3][0];
-    cube[2][1]=cube_save[3][1];
-    cube[3][0]=cube_save[4][0];
-    cube[3][1]=cube_save[4][1];
-    cube[4][0]=cube_save[1][0];
-    cube[4][1]=cube_save[1][1];
-    cube[1][0]=cube_save[2][0];
-    cube[1][1]=cube_save[2][1];
+    cube[4][1]=cube_save[4][3];
+    cube[4][0]=cube_save[4][0];
+    cube[4][3]=cube_save[4][2];
+    cube[4][2]=cube_save[4][0];
+    cube[0][1]=cube_save[3][1];
+    cube[0][0]=cube_save[3][0];
+    cube[3][1]=cube_save[2][1];
+    cube[3][0]=cube_save[2][0];
+    cube[2][1]=cube_save[1][1];
+    cube[2][0]=cube_save[1][0];
+    cube[1][1]=cube_save[0][1];
+    cube[1][0]=cube_save[0][0];
 }
 void main(){
     turn_Ty_minus();
+    turn_Tx_Plus();
     for (int i = 0; i < 6; i++)
     {
         for (int j = 0; j < 4; j++)
